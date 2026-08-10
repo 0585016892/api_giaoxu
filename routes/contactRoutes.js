e8be7a41f0731d81af87c3bad359bd189a9ad16a
Hung <tranhung6829@gmail.com>
2026-08-10 17:23:25 +0700

    update api 15:58

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -18,7 +18,8 @@ router.post("/", async (req, res) => {
         pass: process.env.EMAIL_PASS,
       },
     });
-
+    console.log("EMAIL_USER =", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS =", process.env.EMAIL_PASS ? "CO" : "KHONG");
     await transporter.sendMail({
       from: `"${name}" <${process.env.EMAIL_USER}>`,
       to: "giaoxudongquan@gmail.com",
